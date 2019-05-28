57c906a2a4ab10009d266f894ac4a1521b1c66ee
Shahar Solomianik <shahar@gnift.com>
2019-05-28 15:16:50 +0300

    basic isolation query works

--- models/missions.js
+++ models/missions.js
@@ -1,5 +1,57 @@
 module.exports = {
-  getCountriesByIsolation: function(db){
+  getAll: async function(db){
+    let missions = db.get('missions');
+    let all = await missions.find({});
+    return all;
+  },
+  getIsolatedAgents: async function(db){
+    let missions = db.get('missions');
+    let isolatedAgents = missions.aggregate([
+      {
+        $group: {
+          _id: "$agent",
+          count: {$sum: 1}
+        }
+      },
+      {
+        $match: {
+          count: {$eq: 1}
+        }
+      }
+    ])
+  },
+  getMostIsolatedCountry: function(db){
+    let missions = db.get('missions');
+    let result = missions.aggregate([
+      {
+        $group: {
+          _id: "$agent",
+          count: {$sum: 1},
+          country: {$first: "$country"}
+        }
+      },
+      {
+        $match: {
+          count: {$eq: 1}
+        }
+      },
+      {
+        $group: {
+          _id: "$country",
+          count: {$sum: 1}
+        }
+      },
+      {
+        $sort: {
+          count: -1
+        }
+      },
+      {
+        $limit: 1
+      }
+    ])
+
+    return result;
 
   },
   getClosest: function (db,target){
--- routes/missions.js
+++ routes/missions.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const util = require('util')
 
 const missions = require('../models/missions')
 
 /* GET users listing. */
 router.get('/countries-by-isolation', function(req, res, next) {
-  missions.getCountriesByIsolation(req.db)
+  missions.getMostIsolatedCountry(req.db)
   .then((data) => {
-    res.json(data)
+    console.log(util.inspect(data))
+    res.json({
+      country: data[0]._id,
+      isolation_level: data[0].count
+    })
   })
   .catch((err) => {
     res.status(500).send(err)
